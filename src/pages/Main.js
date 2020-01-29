f84607402119559f4418a1e539b94daebfa3b945
Alexandre de Oliveira Pereira <alexandre.olipe@gmail.com>
2020-01-29 00:05:42 -0300

    Adicionada callout para descricao de marker

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image, View, Text } from 'react-native';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, Callout } from 'react-native-maps';
 
 function Main() {
 
@@ -39,6 +39,14 @@ function Main() {
         <MapView initialRegion={currentRegion} style={styles.map}>
             <Marker coordinate={{ latitude: -25.40, longitude: -49.20}}>
                 <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/34045338?v=4'}}/>
+
+                <Callout>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Alexandre Pereira</Text>
+                        <Text style={styles.devBio}>Apaixonado por tecnologia e sempre em busca de novos conhecimentos</Text>
+                        <Text style={styles.devTechs}>Java, Angular, JavaScript, SQL</Text>
+                    </View>
+                </Callout>
             </Marker>
         </MapView>
     );
@@ -55,6 +63,24 @@ const styles = StyleSheet.create({
         borderRadius: 4,
         borderWidth: 4,
         borderColor: '#FFF'
+    },
+
+    callout: {
+        width: 260,
+    },
+
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+
+    devBio: {
+        color: '#666',
+        marginTop: 5,
+    },
+
+    devTechs: {
+        marginTop: 5,
     }
 });
 
