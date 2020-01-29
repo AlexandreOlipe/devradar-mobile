f8851fcd9b481719ed6ee71d3e9104a76da49271
Alexandre de Oliveira Pereira <alexandre.olipe@gmail.com>
2020-01-28 23:39:02 -0300

    Adicionado mapa

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,29 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import Main from './pages/Main';
-import Profiles from './pages/Profile';
+import Profile from './pages/Profile';
 
 const Routes = createAppContainer(
     createStackNavigator({
-        Main,
-        Profiles
+        Main: {
+            screen: Main,
+            navigationOptions: {
+                title: 'DevRadar'
+            }
+        },
+        Profile: {
+            screen: Profile,
+            navigationOptions: {
+                title: 'Perfil Github'
+            }
+        }
+    }, {
+        defaultNavigationOptions: {
+            headerTintColor: '#FFF',
+            headerStyle: {
+                backgroundColor: '#7d40e7'
+            }
+        }
     })
 );
 
